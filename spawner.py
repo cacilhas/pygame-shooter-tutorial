d4c0a010cf1401b4e89e969f5919e663a68f9539
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-11-15 00:24:39 -0300

    better spawning time

--- spawner.py
+++ spawner.py
@@ -6,8 +6,16 @@ from foe import Foe
 
 class Spawner(Actor):
 
+    def __init__(self) -> None:
+        self.wait_time: float = 0.0
+        self.max_wait_time: float = 5.0
+
     async def update(self, delta: float) -> Action:
-        if random() < 2 ** -7:
+        self.wait_time -= delta
+        self.wait_time = max(0.0, self.wait_time)
+        if self.wait_time == 0:
+            self.wait_time = random() * self.max_wait_time
+            self.max_wait_time -= random() / 10
             y = randint(10, RESOLUTION[1] - 10)
             speed = 200 + random() * 200
             return Action.register(Foe(y, speed))
